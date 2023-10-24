68ae7da21f99d6c3b350a78d7ab519635e23afd2
salawu <a.salawu@outlook.com>
2023-10-24 01:41:04 +0000

    Search bar added

--- client/src/pages/Home/HomePage.js
+++ client/src/pages/Home/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useReducer } from 'react'
-import { getAll } from '../../services/foodService';
+import { getAll, search } from '../../services/foodService';
 import Thumbnails from '../../components/Thumbnails/Thumbnails';
+import { useParams } from 'react-router-dom';
+import Search from '../../components/Search/Search';
 
 const initialState = { foods: [] }
 const reducer = (state, action) => {
@@ -11,18 +13,21 @@ const reducer = (state, action) => {
             return state;
     }
 }
+
 export default function HomePage() {
     const [state, dispatch] = useReducer(reducer, initialState)
     const { foods } = state;
+    const { searchTerm } = useParams()
 
     // getting our state and updating the initial state
     useEffect(() => {
-        getAll()
-            .then((foods) => dispatch({type: 'FOODS_LOADED', payload: foods}))
-    }, [])
+        const loadedFoods = searchTerm ? search(searchTerm) : getAll()
+            loadedFoods.then((foods) => dispatch({type: 'FOODS_LOADED', payload: foods}))
+    }, [searchTerm])
     
   return (
       <div>
+          <Search />
           <Thumbnails foods={foods} />
       </div>
   )
--- client/src/services/foodService.js
+++ client/src/services/foodService.js
@@ -1,3 +1,9 @@
 import { sample_foods } from "../data";
 
 export const getAll = async () => sample_foods
+
+// to lowercase makes the search case insensitive
+export const search = async (searchTerm) => 
+    sample_foods.filter(item => 
+        item.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+    )
