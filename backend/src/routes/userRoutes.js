6e0eb1fdb55a03b012875946dd34ade90b3b7f10
salawu <a.salawu@outlook.com>
2023-10-31 01:52:20 +0000

    login route added to backend

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { sample_users } from "../../data";
+import jwt from 'jsonwebtoken'
+import { BAD_REQUEST } from "../constants/httpStatus";
+
+const router = Router()
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+    const user = sample_users.find(
+        user => user.email === email && user.password === password
+    )
+    if (user) {
+        res.send(generateResponseToken(user))
+        return
+    }
+    res.status(BAD_REQUEST).send('username or email is invalid')
+})
+
+const generateResponseToken = (user) => {
+    const token = jwt.sign(
+        {
+            id: id,
+            email: user.email,
+            isAdmin: user.isAdmin
+        },
+        'somerandomtext',
+        {
+            expiresIn: '30d'
+        }
+    )
+    return {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        isAdmin: user.isAdmin,
+        address: user.address,
+        token
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import foodRouter from './routes/foodRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 app.use(
@@ -9,8 +10,10 @@ app.use(
         origin: ['http://localhost:3001']
     })
 )
+app.use(express.json())
 
 app.use('/api/foods', foodRouter)
+app.use('/users', userRouter)
 const PORT = 5000
 app.listen(PORT, () => {
     console.log('listening on port '+ PORT);
